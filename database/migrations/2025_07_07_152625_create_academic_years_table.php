59a30a8902fc501380315512ceaea4eebe114006
As3ad Altabali <as3ad.moh@gmail.com>
2025-07-13 01:44:04 +0300

    feat: update academic_years migration to allow nullable start_date and end_date fields

--- database/migrations/2025_07_07_152625_create_academic_years_table.php
+++ database/migrations/2025_07_07_152625_create_academic_years_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\AcademicYear;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,12 +15,19 @@ return new class extends Migration
         Schema::create('academic_years', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique(); // e.g., "2025-2026"
-            $table->date('start_date'); // Start date of the academic year
-            $table->date('end_date'); // End date of the academic year
+            $table->date('start_date')->nullable(); // Start date of the academic year
+            $table->date('end_date')->nullable(); // End date of the academic year
             $table->boolean('is_active')->default(true); // Indicates if the academic year is currently active
             $table->json('additional_info')->nullable(); // For any extra information related to the academic year, such as holidays, special events, etc.
             $table->timestamps();
         });
+
+        AcademicYear::create([
+            'name' => now()->year . '-' . (now()->year + 1),
+            'start_date' => now()->year . '-09-01',
+            'end_date' => now()->year + 1 . '-06-30',
+            'is_active' => true,
+        ]);
     }
 
     /**
