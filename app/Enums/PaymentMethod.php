cd89feb70d94fd5dcb22ff87ebba50eee6d040dc
As3ad Altabali <as3ad.moh@gmail.com>
2025-07-14 11:05:36 +0300

    feat: implement HasLabel contract in PaymentMethod enum and add label and color methods

--- app/Enums/PaymentMethod.php
+++ app/Enums/PaymentMethod.php
@@ -2,9 +2,27 @@
 
 namespace App\Enums;
 
-enum PaymentMethod: string
+enum PaymentMethod: string implements \Filament\Support\Contracts\HasLabel
 {
     case CASH = 'cash';
     case BANK_TRANSFER = 'bank_transfer';
     case THAWANI = 'thawani';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::CASH => 'Cash',
+            self::BANK_TRANSFER => 'Bank Transfer',
+            self::THAWANI => 'Thawani',
+        };
+    }
+
+    public function colors(): string
+    {
+        return match ($this) {
+            self::CASH => 'gray',
+            self::BANK_TRANSFER => 'info',
+            self::THAWANI => 'success',
+        };
+    }
 }
