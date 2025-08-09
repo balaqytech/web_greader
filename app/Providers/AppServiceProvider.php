b11136fcfc80d279e36e420f39dea85ccf0a11b0
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-10 01:47:10 +0300

    feat: install and configure log viewer package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LogViewer::auth(fn() => Auth::check() && Auth::user()->hasRole('super_admin'));
     }
 }
