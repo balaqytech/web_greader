eea95f7665911c434f0e902b5214be15ba64c1be
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-09 15:53:27 +0300

    feat: update ShieldSeeder to include detailed permissions for super_admin role

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -12,7 +12,7 @@ class ShieldSeeder extends Seeder
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":[]}]';
+        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["view_academic::year","view_any_academic::year","create_academic::year","update_academic::year","delete_academic::year","view_addon","view_any_addon","create_addon","update_addon","delete_addon","view_branch","view_any_branch","create_branch","update_branch","delete_branch","view_discount","view_any_discount","create_discount","update_discount","delete_discount","view_installment","view_any_installment","create_installment","update_installment","delete_installment","view_parent::account","view_any_parent::account","create_parent::account","update_parent::account","delete_parent::account","view_payment","view_any_payment","create_payment","update_payment","delete_payment","view_program","view_any_program","create_program","update_program","delete_program","view_program::enrollment","view_any_program::enrollment","create_program::enrollment","update_program::enrollment","delete_program::enrollment","view_role","view_any_role","create_role","update_role","delete_role","delete_any_role","view_student","view_any_student","create_student","update_student","delete_student","view_user","view_any_user","create_user","update_user","ban_user","unban_user"]}]';
         $directPermissions = '[]';
 
         static::makeRolesWithPermissions($rolesWithPermissions);
