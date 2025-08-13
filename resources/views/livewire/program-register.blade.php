5d16c91a830882356d48352e4ed4a9ae370d1e67
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-13 04:56:34 +0300

    chore: fix register styles

--- resources/views/livewire/program-register.blade.php
+++ resources/views/livewire/program-register.blade.php
@@ -2,14 +2,14 @@
     <section>
         <div class="wrapper relative min-h-[80vh]">
             <!-- Vimeo video embed -->
-            <iframe src="https://player.vimeo.com/video/1086378824?title=0&byline=0&portrait=0&autoplay=1"
+            <iframe src="https://player.vimeo.com/video/1086378824?title=0&byline=0&portrait=0&autoplay=1&muted=0"
                 class="absolute left-0 top-0 h-full w-full" frameborder="0"
                 allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
             </iframe>
         </div>
     </section>
 
-    <section class="py-24">
+    <section class="pt-24">
         <div class="wrapper prose prose-slate">
             <h1 class="text-center text-3xl font-bold text-gr-rose">
                 سجل أطفالك معنا دون التزام
@@ -27,8 +27,12 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                 </svg>
-            </div>
+            </div>            
+        </div>
+    </section>
 
+    <section class="pb-24">
+        <div class="wrapper">
             <div class="mx-auto mt-12 list-none">
                 <form wire:submit="create">
                     {{ $this->form }}
