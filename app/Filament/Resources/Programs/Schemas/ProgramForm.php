0da8bf4ba197c402606976b96b2ad102192fe755
As3ad Altabali <as3ad.moh@gmail.com>
2026-04-18 10:25:13 +0300

    fix: add min & max birth date to program resource

--- app/Filament/Resources/Programs/Schemas/ProgramForm.php
+++ app/Filament/Resources/Programs/Schemas/ProgramForm.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Programs\Schemas;
 
 use App\Enums\ProgramType;
 use App\Models\Branch;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
@@ -20,6 +21,8 @@ class ProgramForm
         return $schema
             ->components([
                 Section::make(__('admin.program.program_info'))
+                    ->columns(2)
+                    ->columnSpanFull()
                     ->schema([
                         TextInput::make('name')
                             ->label(__('admin.program.name'))
@@ -28,31 +31,26 @@ class ProgramForm
                             ->label(__('admin.program.type'))
                             ->options(ProgramType::class)
                             ->required(),
-                        TextInput::make('base_price')
-                            ->label(__('admin.program.base_price'))
-                            ->required()
-                            ->numeric()
-                            ->default(0.0)
-                            ->prefix('ر.ع'),
+                        DatePicker::make('min_birth_date')
+                            ->label(__('admin.program.min_birth_date'))
+                            ->required(),
+                        DatePicker::make('max_birth_date')
+                            ->label(__('admin.program.max_birth_date'))
+                            ->required(),
                         Toggle::make('accept_installments')
                             ->label(__('admin.program.accept_installments'))
                             ->default(false)
+                            ->inline(false)
+                            ->required(),
+                        Toggle::make('is_open')
+                            ->label(__('admin.program.is_open'))
+                            ->default(true)
                             ->inline(false)
                             ->required(),
                         Textarea::make('description')
                             ->label(__('admin.program.description'))
                             ->default(null)
                             ->columnSpanFull(),
-                        RichEditor::make('contract')
-                            ->label(__('admin.program.contract'))
-                            ->default(null)
-                            ->columnSpanFull()
-                            ->helperText(__('admin.program.contract_helper_text')),
-                        Toggle::make('is_open')
-                            ->label(__('admin.program.is_open'))
-                            ->default(true)
-                            ->inline(false)
-                            ->required(),
                         Toggle::make('is_active')
                             ->label(__('admin.program.is_active'))
                             ->default(true)
@@ -65,9 +63,12 @@ class ProgramForm
                             ->default(0),
                     ]),
                 Section::make(__('admin.program.branches_info'))
+                    ->columnSpanFull()
                     ->schema([
                         Repeater::make('branches')
                             ->label(__('admin.program.branches'))
+                            ->columns(2)
+                            ->columnSpanFull()
                             ->schema([
                                 Select::make('branch_id')
                                     ->label(__('admin.program.branch'))
@@ -81,6 +82,15 @@ class ProgramForm
                                     ->default(0.0)
                                     ->prefix('ر.ع'),
                             ]),
+                    ]),
+                Section::make(__('admin.program.contract'))
+                    ->columnSpanFull()
+                    ->schema([
+                        RichEditor::make('contract')
+                            ->label(__('admin.program.contract'))
+                            ->default(null)
+                            ->columnSpanFull()
+                            ->helperText(__('admin.program.contract_helper_text')),
                     ]),
             ]);
     }
